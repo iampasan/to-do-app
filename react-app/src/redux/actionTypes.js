1b24552ecd40460c2738260b02d2e8f249d949d6
pasan <pasans96@gmail.com>
2019-07-24 10:11:31 +0530

    ADD:Sign in sign out to redux store

--- react-app/src/redux/actionTypes.js
+++ react-app/src/redux/actionTypes.js
@@ -2,4 +2,5 @@ export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 export const SET_FILTER = "SET_FILTER";
 export const LOAD_TODOS = "LOAD_TODOS";
+export const SET_USER = "SET_USER";
 
