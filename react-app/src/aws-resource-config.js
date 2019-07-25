222fb0bf2d0e6039ced2c42184fa8fb39e426ec6
pasan <pasans96@gmail.com>
2019-07-25 12:01:15 +0530

    UPDATE : AWS resource config

--- react-app/src/aws-resource-config.js
+++ react-app/src/aws-resource-config.js
@@ -1,17 +1,17 @@
 export default {
     apiGateway: {
         REGION: "ap-southeast-1",
-        URL: "https://1pz5vn7ll6.execute-api.ap-southeast-1.amazonaws.com/dev/api"
+        URL: "https://bmyhcj7d37.execute-api.ap-southeast-1.amazonaws.com/dev/api"
         //URL:"https://d1aq9uhbhb07yr.cloudfront.net/dev/api"
     },
     cognito: {
         REGION: "ap-southeast-1",
-        USER_POOL_ID: "ap-southeast-1_KpxvpSu11",
-        APP_CLIENT_ID: "2ae1srf3qnhhl0mstiaqjdbp32",
-        IDENTITY_POOL_ID: "ap-southeast-1:adbf58ea-6262-45e6-97e3-8b49927623d9"
+        USER_POOL_ID: "ap-southeast-1_WyB6qKQsu",
+        APP_CLIENT_ID: "67gb4v70mhmpi3dgrpocj5a983",
+        IDENTITY_POOL_ID: "ap-southeast-1:4790d7ff-ebec-460d-b04c-ad55bed7cc57"
     },
     oauth : {
-        domain: 'to-do.auth.ap-southeast-1.amazoncognito.com',
+        domain: 'to-do-dev-auth.auth.ap-southeast-1.amazoncognito.com',
         scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
         redirectSignIn: window.location.origin,
         redirectSignOut: window.location.origin,
